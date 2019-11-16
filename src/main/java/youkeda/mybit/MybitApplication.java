c219dfa3ca0b07eb921aed1e08035d1b7994f791
ZZKSZYJDD <51317988+ZZKSZYJDD@users.noreply.github.com>
2019-11-16 17:59:26 +0800

    Initial commit

---
+++ src/main/java/youkeda/mybit/MybitApplication.java
@@ -0,0 +1,13 @@
+package youkeda.mybit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybitApplication.class, args);
+	}
+
+}
